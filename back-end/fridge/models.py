d46712c9cd26fa4317fb907104b775a9174c2f5a
Fergus Lai <ferguslai724@gmail.com>
2022-11-19 16:08:45 +0000

    Django Setup

---
+++ back-end/fridge/models.py
@@ -0,0 +1,36 @@
+# Copyright 2015 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from django.db import models
+
+
+class User(models.Model):
+    id = models.CharField(max_length=200)
+    name = models.TextField()
+
+
+class Freezer_Item(models.model):
+    id = models.CharField(max_length=200)
+    name = models.TextField()
+    type = models.TextField()
+    exp_date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Fridge_Item(models.model):
+    id = models.CharField(max_length=200)
+    name = models.TextField()
+    type = models.TextField()
+    exp_date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
