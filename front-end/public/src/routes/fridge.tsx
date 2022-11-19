f060d1d66c9bbb60967cccab4fc14781998b9571
Fergus Lai <ferguslai724@gmail.com>
2022-11-19 23:20:29 +0000

    variable for login logout and authenticated

--- front-end/public/src/routes/fridge.tsx
+++ front-end/public/src/routes/fridge.tsx
@@ -1,6 +1,10 @@
+import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export function Fridge() {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
   const { unityProvider } = useUnityContext({
     loaderUrl: "unity/Build.loader.js",
     dataUrl: "unity/Build.data",
