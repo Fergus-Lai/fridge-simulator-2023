3c264e4efc865bc2391070515cf374518a225d58
WNP78 <wnp78g@gmail.com>
2022-11-19 17:27:46 +0000

    Shelves that slide out on click

--- fridge-unity/Assets/Scripts/KeepBoxInFrame.cs
+++ fridge-unity/Assets/Scripts/KeepBoxInFrame.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class KeepBoxInFrame : MonoBehaviour
 {
-    Camera camera;
+    private new Camera camera;
     public GameObject target;
 
     [Range(0.8f, 1.5f)]
@@ -14,7 +14,8 @@ public class KeepBoxInFrame : MonoBehaviour
     public float MaxLerpSpeed = 2f;
     public float MoveTime = 0.5f;
     public float RotationSpeed = 0.01f;
-
+    public ViewAngle angle;
+    public ViewDirection direction;
     private Vector3 smoothVel;
     void Start ()
     {
@@ -30,18 +31,56 @@ public class KeepBoxInFrame : MonoBehaviour
             bounds.Encapsulate(renderer.bounds.max);
         }
 
-        float width = bounds.size.x * PadFactor, height = bounds.size.y * PadFactor;
+        foreach (var collider in target.GetComponentsInChildren<Collider>())
+        {
+            bounds.Encapsulate(collider.bounds.min);
+            bounds.Encapsulate(collider.bounds.max);
+        }
+
+        Quaternion rotation = direction switch {
+            ViewDirection.Front => Quaternion.identity,
+            ViewDirection.Top => Quaternion.LookRotation(Vector3.down, Vector3.left),
+            _ => Quaternion.identity,
+        };
+
+        (float width, float height) = direction switch {
+            ViewDirection.Front => (bounds.size.x, bounds.size.y),
+            ViewDirection.Top => (bounds.size.z, bounds.size.x),
+            _ => (bounds.size.x, bounds.size.y),
+        };
+
         float gY = Mathf.Tan(Mathf.Deg2Rad * camera.fieldOfView);
         float gX = Mathf.Tan(Mathf.Deg2Rad * camera.fieldOfView * camera.aspect);
         float dist = Mathf.Max(width / gX, height / gY);
         dist = Mathf.Max(dist, camera.nearClipPlane);
 
         Vector3 pos = bounds.center;
-        pos.z = bounds.min.z - dist;
+
+        if (angle.HasFlag(ViewAngle.Top)) pos.y = bounds.max.y;
+        else if (angle.HasFlag(ViewAngle.Bottom)) pos.y = bounds.min.y;
+
+        if (angle.HasFlag(ViewAngle.Left)) pos.x = bounds.min.x;
+        else if (angle.HasFlag(ViewAngle.Right)) pos.x = bounds.max.x;
+
+        if (direction == ViewDirection.Front)
+        {
+            pos.z = bounds.min.z - dist;
+        }
+        else if (direction == ViewDirection.Top)
+        {
+            pos.y = bounds.max.y + dist;
+        }
+
+        Vector3 up = direction switch {
+            ViewDirection.Front => Vector3.up,
+            ViewDirection.Top => Vector3.left,
+            _ => Vector3.up,
+        };
+
         transform.position = SmoothDamp(transform.position, pos, ref smoothVel, MoveTime, MaxLerpSpeed);
         transform.rotation = Quaternion.Slerp(
             transform.rotation,
-            Quaternion.LookRotation(bounds.center - transform.position, Vector3.up),
+            Quaternion.LookRotation(bounds.center - transform.position, up),
             1f - Mathf.Pow(0.5f, RotationSpeed * Time.deltaTime));
     }
 
@@ -53,4 +92,19 @@ public class KeepBoxInFrame : MonoBehaviour
         res.z = Mathf.SmoothDamp(current.z, target.z, ref currentVelocity.z, smoothTime, maxSpeed);
         return res;
     }
+}
+
+[System.Flags]
+public enum ViewAngle
+{
+    Center = 0,
+    Top = 1 << 0,
+    Bottom = 1 << 1,
+    Left = 1 << 2,
+    Right = 1 << 3,
+}
+
+public enum ViewDirection
+{
+    Front, Top
 }
---
+++ fridge-unity/Assets/Scripts/ShelfScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShelfScript : MonoBehaviour, IPointerClickHandler
+{
+    public KeepBoxInFrame cameraController;
+    public GameObject mainFridge;
+    bool isExtended = false;
+    private Vector3 initialPos;
+    public Vector3 extension;
+    public float extendTime = 0.5f;
+
+    void Start()
+    {
+        initialPos = transform.localPosition;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!isExtended)
+        {
+            cameraController.target = this.gameObject;
+            cameraController.direction = ViewDirection.Top;
+            isExtended = true;
+            StartCoroutine(AnimateSlide(extension, extendTime));
+        }
+        else
+        {
+            cameraController.target = mainFridge;
+            cameraController.direction = ViewDirection.Front;
+            isExtended = false;
+            StartCoroutine(AnimateSlide(Vector3.zero, extendTime));
+        }
+    }
+
+    private IEnumerator AnimateSlide(Vector3 pos, float t)
+    {
+        pos += initialPos;
+        Vector3 start = transform.localPosition;
+        float et = Time.time + t;
+        while (et > Time.time)
+        {
+            transform.localPosition = Vector3.Lerp(start, pos, Mathf.SmoothStep(1f, 0f, (et - Time.time) / t));
+            yield return null;
+        }
+
+        transform.localPosition = pos;
+    }
+}
