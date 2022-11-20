aea668571fec52e6a44689526f179fed7b8cd5bd
WNP78 <wnp78g@gmail.com>
2022-11-20 10:13:28 +0000

    Ready to merge

--- front-end/src/routes/fridge.tsx
+++ front-end/src/routes/fridge.tsx
@@ -1,19 +1,56 @@
 import { useEffect } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
+const url : string = "http://localhost:8080/";
+
 function sendMsg(obj : string, func : string, arg : string)
 {
   (window as any).unityInstance.SendMessage(obj, func, arg);
 }
 
-function removeItem(itemId : string)
+async function removeItem(itemId : string)
 {
   console.log("Removing " + itemId);
+  let resp = await fetch(url + "/fridge",
+  {
+    method: "DEL",
+    body: JSON.stringify({id: itemId}),
+  });
+
+  if (!resp.ok) console.log(await resp.text());
 }
 
-function addItem(name : string, type : string, id : string, shelf : string)
+async function addItem(name : string, type : string, id : string, shelf : string, date : string)
 {
-  console.log("Adding item " + name + " type " + type + " id " + id + " shelf " + shelf);
+  console.log("Adding item " + name + " type " + type + " id " + id + " shelf " + shelf + " date " + date);
+  let resp = await fetch(url + "/fridge",
+  {
+    method: "POST",
+    body: JSON.stringify({name: name, id: id, type: type, expDate: date, quantity: 1}),
+  });
+
+  if (!resp.ok) console.log(await resp.text());
+}
+
+async function init(addEventListener : any)
+{
+  let resp = await fetch(url + "/fridges");
+
+  if (resp.ok)
+  {
+    let dat = await resp.json();
+    let data = {items: dat.fridge};
+    sendMsg("fridge", "AddItems", JSON.stringify(data));
+  }
+
+  addEventListener("AddItem", (data : any) => {
+    let dat = JSON.parse(data);
+    addItem(dat.name, dat.type, dat.id, dat.shelf, dat.date);
+  });
+
+  addEventListener("RemoveItem", (data : any) => {
+    removeItem(data);
+  });
 }
 
 export function Fridge() {
@@ -50,14 +87,8 @@ export function Fridge() {
       },
     ]}));
 
-    addEventListener("AddItem", (data) => {
-      let dat = JSON.parse(data);
-      addItem(dat.name, dat.type, dat.id, dat.shelf);
-    });
-
-    addEventListener("RemoveItem", (data) => {
-      removeItem(data);
-    });
+    init(addEventListener);
+    
   });
   
   return <Unity unityProvider={unityProvider} className="unity-canvas" />;
